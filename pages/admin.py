d85748a64eda7274b0c15c218f1d2ebcea005523
Emin Mastizada <emin@mastizada.com>
2016-10-05 17:50:46 +0300

    page admin, jquery, background image, logo

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from pages.models import Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+
+admin.site.register(Page, PageAdmin)
